0991e08ea2413c95ad0f5a4b9737e4aef5aef95e
TokyoOwl <121123083+TokyoOwl@users.noreply.github.com>
2023-02-24 17:41:37 +0300

    Update fibbonachi.cpp

--- fibbonachi.cpp
+++ fibbonachi.cpp
@@ -9,16 +9,6 @@ int main()
     int fib, n;
     cout << "Vvedite nomer chisla fibbonachi" << endl;
     cin >> n;
-    if (n=1)
-    {
-        cout << "Chislo: " << fib1 << endl;
-    }
-    else if (n=2) 
-    {
-        cout << "Chislo: " << fib2 << endl;
-    }
-    if (n > 2)
-    {
     for (int i=0; i<n; i++)
     {
         fib = fib1 + fib2;
@@ -26,6 +16,5 @@ int main()
         fib2 = fib;
     }
     cout << "Chislo: " << fib << endl;
-    }
     return 0;
 }
