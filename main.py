5faa4e90cacb40f37d3de3a3356dc6431c36d449
TokyoOwl <wot291@gmail.com>
2023-04-01 19:19:04 +0300

    Артём Добавил main.py это простенький калькулятор на питоне

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Test message 1")
-print("Test message 2")
+while True:
+    print("Введите первое число")
+    s = int(input())
+    print("Введите второе число")
+    s1 = int(input())
+    print("Введите операцию")
+    s2 = input()
+    if s2 == "+":
+        s3 = s + s1
+        print(f"Результат = {s3}")
+    if s2 == "-":
+        s3 = s - s1
+        print(f"Результат = {s3}")
+    if s2 == "*":
+        s3 = s * s1
+        print(f"Результат = {s3}")
+    if s2 == "/":
+        s3 = s / s1
+        print(f"Результат = {s3}")
+    if s2 == "~":
+        break
